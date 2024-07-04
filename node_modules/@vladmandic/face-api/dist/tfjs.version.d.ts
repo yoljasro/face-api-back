d8e6099abb7278ff16c17f85a583ab0816c5c36a
yoljasro <saidaliyevjasur450@gmail.com>
2024-07-04 11:59:29 +0500

    changes

--- node_modules/@vladmandic/face-api/dist/tfjs.version.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const version: {
-  'tfjs-core': string;
-  'tfjs-backend-cpu': string;
-  'tfjs-backend-webgl': string;
-  'tfjs-data': string;
-  'tfjs-layers': string;
-  'tfjs-converter': string;
-  tfjs: string;
-};
--- node_modules/@vladmandic/face-api/src/dom/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Tensor3D, Tensor4D } from '../../dist/tfjs.esm';
-
-import { NetInput } from './NetInput';
-
-export type TMediaElement = HTMLImageElement | HTMLVideoElement | HTMLCanvasElement
-
-export type TResolvedNetInput = TMediaElement | Tensor3D | Tensor4D
-
-export type TNetInput = string | TResolvedNetInput | Array<string | TResolvedNetInput> | NetInput
